2a71450e49990821b60782b289d8fcbcced64120
jlg <66569721+J-jlg@users.noreply.github.com>
2024-04-07 15:10:34 +0200

    feat: code snippet docu

---
+++ tests/crackme_safe.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h> 
+
+int main ( int argc, char *argv[] ) 
+{ 
+   char input [4] ; 
+   printf( "Enter the combination of the safe: " ) ; 
+   if (fgets(input, 4, stdin) == NULL)
+   { 
+   	puts( "error" ); 
+   	return -1; 
+   } 
+
+   if(input[0]==’4’ && input[1]==’8’&&input[2]==’8’&&input[3]==’9’) 
+   	puts( "secret" ); 
+   else 
+   	puts( "wrong combination" ); 
+   return 0;  
+} 
+
+
+//strcmp
+if(!strcmp(input,”test”) { 
+	puts("wrong"); 
+	return -1; 
+} 
+
+if(!strcmp(sha3_b64(input), "NTJhZTg5YmUzMjA2MTI0NTQyMjM2ZjAxZDU1ZDBlMjIzNjQxM2M3YjU5MDJkOTZlNjU2ZWMyYmIwZjE0OWZkOWY5NDVjYWEyZTRiMzRiNWU2NWEyMGNlZmQzYzAzMTBmYTE2Y2E1YmI4MDAyNjZmNjRkMTUwY2RlYTlkYTY1MmM=") { 
+	puts("wrong"); 
+	return –1; 
+} 
+
+puts("wrong") -> puts(sha3_256("wrong")); 
+
+const char *solutionString = "kbwb/cjcmf "; 
+
+... 
+
+if(stringArray[i] != solutionString[i]-1){ 
+	puts("\nwrong"); 
+	return -1; 
+.. 
+} 
+
+  %29 = getelementptr inbounds [11 x i8], ptr %6, i64 0, i64 2 
+  %30 = load i8, ptr %29, align 1 
+  %31 = sext i8 %30 to i32 
+  %32 = icmp eq i32 %31, 118 
+  br i1 %32, label %33, label %77 
